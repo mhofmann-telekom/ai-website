026e1e16e10eaabb156fb78fbdf5fd632d18e381
Marvin <hofmann.marvin03@gmail.com>
2024-07-30 06:02:53 +0200

    ci: added anchor tag link to navbar contact item

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -49,10 +49,10 @@ export default function Sidebar(){
           <svg data-icon-name="user-10" data-style="line" icon_origin_id="24291" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="user-10" className="icon line" width="18" height="18"><path style={{fill: "var(--dark-950)", stroke: "tranparent", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "1.5"}} d="M16.66,12.77A5.93,5.93,0,0,0,17.92,8,6,6,0,0,0,6,9a5.94,5.94,0,0,0,1.34,3.77,1,1,0,0,1-.28,1.45A7,7,0,0,0,4,20a1,1,0,0,0,1,1H19a1,1,0,0,0,1-1,7,7,0,0,0-3.06-5.78A1,1,0,0,1,16.66,12.77Z" id="primary"></path></svg>
           <span>Contact</span>
         </Link> */}
-        <div className="nav-item">
+        <a href="/#contact" className="nav-item">
           <svg data-icon-name="user-10" data-style="line" icon_origin_id="24291" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="user-10" className="icon line" width="18" height="18"><path style={{fill: "var(--dark-950)", stroke: "tranparent", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "1.5"}} d="M16.66,12.77A5.93,5.93,0,0,0,17.92,8,6,6,0,0,0,6,9a5.94,5.94,0,0,0,1.34,3.77,1,1,0,0,1-.28,1.45A7,7,0,0,0,4,20a1,1,0,0,0,1,1H19a1,1,0,0,0,1-1,7,7,0,0,0-3.06-5.78A1,1,0,0,1,16.66,12.77Z" id="primary"></path></svg>
           <span>Contact</span>
-        </div>
+        </a>
       </div>
       <p className="bottom-text">Located in Singapore</p>
     </div>
