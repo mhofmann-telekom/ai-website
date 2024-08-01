f7d3536b4241c5b4f794f2ea99aafb9d771f2ebb
Marvin <hofmann.marvin03@gmail.com>
2024-08-01 10:54:10 +0200

    ci: added further sections

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { data } from '../utils/data';
 import * as THREE from 'three';
-import gsap from 'gsap';
+
 import Tubes from '../components/Tubes/Tubes';
 import Particles from '../components/Particles/Particles';
 import Sidebar from '../components/Sidebar/Sidebar';
@@ -12,6 +12,7 @@ import arrow from "./../assets/icons/arrow-right.svg";
 import Contact from '../components/Contact/Contact';
 import './App.css';
 import Expertise from '../components/Expertise/Expertise';
+import gsap from 'gsap';
 
 const PATHS = data.economics[0].paths;
 const brainCurves = [];
@@ -21,6 +22,26 @@ PATHS.forEach((path) => {
     points.push(new THREE.Vector3(path[i], path[i + 1], path[i + 2]));
   }
   brainCurves.push(new THREE.CatmullRomCurve3(points));
+});
+
+const PATHS2 = data.biology[0].paths;
+const biologyCurves = [];
+PATHS2.forEach((path) => {
+  const points = [];
+  for (let i = 0; i < path.length; i += 3) {
+    points.push(new THREE.Vector3(path[i], path[i + 1], path[i + 2]));
+  }
+  biologyCurves.push(new THREE.CatmullRomCurve3(points));
+});
+
+const PATHS3 = data.engineering[0].paths;
+const crypto = [];
+PATHS3.forEach((path) => {
+  const points = [];
+  for (let i = 0; i < path.length; i += 3) {
+    points.push(new THREE.Vector3(path[i], path[i + 1], path[i + 2]));
+  }
+  crypto.push(new THREE.CatmullRomCurve3(points));
 });
 
 function App() {
@@ -51,7 +72,6 @@ function App() {
         { duration: 1, y: 0, opacity: 1, delay: .1, ease: 'power2.out'}
       );
     });
-
   }, []);
 
   return (
@@ -87,7 +107,7 @@ function App() {
             </div>
           </div>
           <div className="button-wrapper">
-            <a href="#contact" className="secondary-button">
+            <a href="#use-cases" className="secondary-button">
               <span>See our use cases</span>
             </a>
             <span>or</span>
@@ -102,7 +122,7 @@ function App() {
             <rect x="0" y="0" width="200%" height="100%" rx="29" fill="var(--dark-300)" />
           </svg> */}
           <svg id="shape" width="1513" height="1507" viewBox="0 0 1513 1507" xmlns="http://www.w3.org/2000/svg">
-            <rect width="1513" height="1507" rx="92" fill="var(--dark-300)"/>
+            <rect width="1513" height="1507" rx="92"/>
           </svg>
           <div className="canvas-container">
             <Canvas
@@ -125,6 +145,39 @@ function App() {
         <Slider></Slider>
       </div>
       <Expertise></Expertise>
+      <div id="use-cases">
+        <div>
+          <h2 className="headline-h2">Our use cases and products</h2>
+          <p className="headline-subtext margin-y-2rem">In this section, we introduce you our use cases for artificial intelligence in healthcare, insurance and manufacturing.</p>
+        </div>
+        <div id="healthcare">
+          {/* <h3 className="headline-h3"></h3> */}
+        </div>
+        <Canvas
+          style={{ width: "100%", height: "100%", display: "grid", placeItems: "center" }}
+          pixelratio={window.devicePixelRatio}
+          camera={{ position: [0, 0, 0.4] }}
+        >
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} intensity={1} />
+          <Tubes curves={biologyCurves} />
+          <Particles curves={biologyCurves} />
+          <OrbitControls enableZoom={false} />
+        </Canvas>
+      </div>
+      {/* <div style={{backgroundColor:"black", height: "100dvh"}}>
+        <Canvas
+          style={{ width: "100%", height: "100%", display: "grid", placeItems: "center" }}
+          pixelratio={window.devicePixelRatio}
+          camera={{ position: [0, 0, 0.4] }}
+        >
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 10, 10]} intensity={1} />
+          <Tubes curves={crypto} />
+          <Particles curves={crypto} />
+          <OrbitControls enableZoom={false} />
+        </Canvas>
+      </div> */}
       <Contact></Contact>
       <div id="footer">
         <span>T-Systems © {new Date().getFullYear().toString()} - All Rights Reserved.</span>
